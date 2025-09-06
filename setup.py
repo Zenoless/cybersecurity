2ec76bcb4a386bca84cbdab1ef37289f0d695971
Oo <144467415+Zenoless@users.noreply.github.com>
2025-09-06 20:20:34 +0700

    setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="cybersecurity",
     version="0.1",
     py_modules=["main"],
+    install_requires=[],  # add dependencies here if any
     entry_points={
         "console_scripts": [
             "cybersecurity=main:main",
